aeacc6bbb6ce6d6f8b8e22979ec7fe4e871a4f4b
compact-mira <vantruong.visc@gmail.com>
2024-11-06 16:13:27 -0800

    J: added bullet collision & destruction

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ def main():
                 print("Game Over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.kill()
+                    shot.kill()
+
         screen.fill("black")
 
         for obj in drawable:
